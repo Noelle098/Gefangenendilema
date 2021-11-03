917a5cc026ed869ccb58c9cb7a457f9acde746f9
Sara Dunkel <Sara.Dunkel@trivadis.com>
2021-11-03 11:54:07 +0100

    Gefangenendilemma.java ausgefüllt / Gewinnmatrix.java angelegt

--- src/Gefangenendilemma.java
+++ src/Gefangenendilemma.java
@@ -1,2 +1,23 @@
+import java.util.List;
+
 public class Gefangenendilemma {
+    int round;
+    List<Player> players;
+    Gewinnmatrix gewinnmatrix;
+
+    Gefangenendilemma() {
+        gewinnmatrix = new Gewinnmatrix();
+    }
+
+    Gefangenendilemma(Gewinnmatrix gewinnmatrix) {
+        this.gewinnmatrix = gewinnmatrix;
+    }
+
+    public void initGame(int round, List<Player> players) {
+
+    }
+
+    private void fight(Player p1, Player p2) {
+
+    }
 }
---
+++ src/Gewinnmatrix.java
@@ -0,0 +1,17 @@
+public class Gewinnmatrix {
+    int koopkoop;
+    int koopkoopN;
+    int koopNkoopN;
+
+    Gewinnmatrix() {
+        koopkoop = 1;
+        koopkoopN = 3;
+        koopNkoopN = 2;
+    }
+
+    public Gewinnmatrix(int koopkoop, int koopkoopN, int koopNkoopN) {
+        this.koopkoop = koopkoop;
+        this.koopkoopN = koopkoopN;
+        this.koopNkoopN = koopNkoopN;
+    }
+}
