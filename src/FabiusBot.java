7023649dc0188fe48ec95f3e144366a4229f375b
Fabius Hobler <Fabius.Hobler@Trivadis.com>
2021-11-04 16:31:58 +0100

    Fabius Bot

---
+++ src/FabiusBot.java
@@ -0,0 +1,12 @@
+public class FabiusBot extends Player {
+
+    @Override
+    public boolean play(Player p) {
+        return false;
+    }
+
+    @Override
+    protected void history(boolean reaction) {
+
+    }
+}
