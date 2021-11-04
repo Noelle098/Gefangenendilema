1c5226e2e0f4b7a121a285667b79c1c1698f5ad3
Elisabeth Weinfurtner <Elisabeth.Weinfurtner@trivadis.com>
2021-11-04 16:26:18 +0100

    LisaBot mit neuem Plan

--- src/LisaBot.java
+++ src/LisaBot.java
@@ -1,47 +1,62 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class LisaBot extends  Player {
 
     Player currentPlayer;
-    HashMap<Player, Integer> players = new HashMap<Player, Integer>();
+    HashMap<Player, ArrayList<Boolean>> players = new HashMap<Player, ArrayList<Boolean>>();
     int currentRound;
 
 
     @Override
     public boolean play(Player p) {
         if(players.size() == 0){currentRound = 0;}
+        if(currentRound > 7 && currentRound < 11){
+            return false;
+        }
+        if(currentRound > 19 && currentRound <26){
+            return false;
+        }
+        if(currentRound > 42 && currentRound <51){
+            return false;
+        }
         if(players.containsKey(p)){
             currentPlayer = p;
             currentRound++;
+
             return historyCheck(p);
         }
-        players.put(p,0);
+        players.put(p,new ArrayList<Boolean>());
         currentRound++;
-        return Math.random() < 0.8;
-    }
-
-    private boolean historyCheck (Player p) {
-        if(players.get(p) < 0){
-            return false;
-        }
-        else {
-            return true;
-        }
+        return Math.random() < 0.5;
     }
 
     @Override
-    protected void history(int erg) {
+    protected void history(boolean reaction) {
         if(players.containsKey(currentPlayer)){
-
+            players.get(currentPlayer).add(reaction);
+        }else{
+            players.put(currentPlayer, new ArrayList<Boolean>());
+            players.get(currentPlayer).add(reaction);
+            currentRound++;
         }
-
     }
 
-    public static void main(String[] args) {
+    private boolean historyCheck (Player p) {
+        int n = 0;
+        int m = 0;
 
-        for(int i = 0; i < 5; i ++){
-            boolean bo = Math.random() < 0.8;
-            System.out.println(bo);
+        for (Boolean b : players.get(p)) {
+            if (b) {
+                n++;
+            } else {
+                m++;
+            }
+        }
+        if(n > m){
+            return true;
+        }else{
+            return false;
         }
     }
 }
